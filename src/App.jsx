0bb19e46021bed1a9546f31f92cbb3288e0f1dee
adityasabat34 <147155098+adityasabat34@users.noreply.github.com>
2025-01-21 17:49:05 +0530

    useMemo execution

--- src/App.jsx
+++ src/App.jsx
@@ -1,47 +1,27 @@
-import React from "react";
-import axios from "axios";
-import { useState } from "react";
-import { useEffect } from "react";
+import React, { useEffect } from "react";
+import { useMemo, useState } from "react";
 
 const App = () => {
-  const [showData, setShowData] = useState([]);
-  const getData = async () => {
-    // console.log("hello");
-    const res = await axios.get(
-      "https://picsum.photos/v2/list?page=2&limit=10"
-    );
-    // console.log(res);
-    const data = res.data;
-    setShowData(data);
-    // console.log(data);
+  const [count, setcount] = useState(0);
+  const [dark, setdark] = useState(false);
+
+  const expensiveTask = useMemo(() => {
+    return count * 2;
+  }, [count]);
+
+  const darkMode = {
+    backgroundColor: dark ? "black" : "white",
+    color: dark ? "white" : "black",
   };
-  // console.log(showData);
-  useEffect(() => {
-    getData();
-  }, []);
   return (
-    <div className="p-6">
-      <button
-        onClick={getData}
-        className="bg-teal-400 px-6 py-3 rounded font-semibold text-2xl active:scale-95 "
-      >
-        Get Data
+    <div style={darkMode}>
+      <h1 className="text-2xl text-orange-500 ">Theme</h1>
+      <p>count:{count}</p>
+      <p> Expensive Calc : {expensiveTask}</p>
+      <button className="block" onClick={() => setdark(!dark)}>
+        Switch to dark
       </button>
-      <div className="p-5 bg-gray-950 mt-5 text-teal-500 rounded-2xl">
-        {showData.map((elem, idx) => {
-          return (
-            <div
-              key={idx}
-              className="bg-slate-100 w-full flex py-6 px-6 mb-3 items-center justify-between rounded-md "
-            >
-              <img className="h-40" src={elem.download_url} alt="" />
-              <h1 className="font-semibold text-2xl text-black">
-                {elem.author}
-              </h1>
-            </div>
-          );
-        })}
-      </div>
+      <button onClick={() => setcount(count + 1)}>Count + 1 </button>
     </div>
   );
 };
